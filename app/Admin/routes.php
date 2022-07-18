f3241e697c739432e67935543f5ba86beda9d0f8
渡邊克己 <ktm981124@gmail.com>
2022-07-18 11:24:41 +0900

    laravel-admin導入

---
+++ app/Admin/routes.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+Admin::routes();
+
+Route::group([
+    'prefix'        => config('admin.route.prefix'),
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+    'as'            => config('admin.route.prefix') . '.',
+], function (Router $router) {
+    $router->post('/fileupload', 'FileuploadController@index'); // Add here
+    $router->get('/', 'HomeController@index')->name('home');
+    $router->get('/genre', 'GenreController@index')->name('genre');
+    $router->resource('administrators', AdministratorController::class);
+
+});
---
+++ database/migrations/2022_07_18_011331_add_column_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('kana_name')->nullable();
+            $table->string('address')->nullable();
+            $table->string('zip')->nullable();
+            $table->string('tel')->nullable();
+            $table->boolean('user_status')->nullable()->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
