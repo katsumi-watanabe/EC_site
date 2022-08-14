0838224e23968a66ee95bbef5e838c3346e7b380
渡邊克己 <ktm981124@gmail.com>
2022-08-14 15:50:45 +0900

    like機能一旦保留

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -5,18 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Like;
+use App\Models\User;
 
 class LikeController extends Controller
 {
     public function store($productId)
     {
-        Auth::user()->likes($productId);
-        // $like = new Like;
-        // $like->user_id = Auth::user()->id;
-        // $like->product_id = $productId;
-        // dd(Auth::id());
-        // $like->user_id = Auth::id();
-        // $like->save();
+        $user = Auth::user()->like($productId);
         if (null === $like) {
             // do something here so the code
             // after the block doesn't fail
